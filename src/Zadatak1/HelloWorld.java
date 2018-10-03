a17c27b3a96c3f7101f31ec3b19ed431ce599058
vkrekic-endava <vladimir.krekic@endava.com>
2018-10-03 16:52:07 +0200

    Dodat Zadatak1/HelloWorld

---
+++ src/Zadatak1/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+  ZADATAK1: Kreirati projekat u razvojnom okruženju (Eclipse, IntelliJ)
+  i napisati program koji u konzoli ispisuje tekst: “Hello World!”.
+ */
+
+package Zadatak1;
+
+/**
+ * AUTOR: Vladimir Krekic
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
