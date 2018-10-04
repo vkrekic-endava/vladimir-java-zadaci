300c17425d26a83d68d0209b75e0617e44012d1e
vkrekic-endava <vladimir.krekic@endava.com>
2018-10-04 12:16:26 +0200

    Ispravljeno JavaDoc@.

--- src/Zadatak2/Pogadjanje.java
+++ src/Zadatak2/Pogadjanje.java
@@ -1,68 +1,78 @@
-/*
-  Napisati program za pogađanje slučajno generisanog broja u opsegu od 1 do 50.
-  Program treba da radi na sledeći način: Korisnik preko standardnog ulaza unosi jedan ceo broj.
-  Broj koji treba pogoditi generisati “random” u opsegu od 1 do 50.
-  Vrteti se u petlji dok ne pogodimo broj, ili budemo barem blizu (ako je razlika između brojeva 5 ili manja).
-  Taj deo sa proverom pogotka izdvojiti u zasebnu metodu, kojoj se kao argument prosleđuje broj koji tipujemo,
-  a povratna vrednost metode je 0 – ako smo pogodili, 1 – ako smo bili blizu, 2 – ako smo promašili.
-  U zavisnosti od rezultata pogađanja ispisati i odgovarajuću poruku.
-  Nakon odigrane partije proveriti da li korisnik zeli da prekine ili da nastavi igru.
- */
-
 package Zadatak2;
+
+/**
+ *   Napisati program za pogađanje slučajno generisanog broja u opsegu od 1 do 50.
+ *   Program treba da radi na sledeći način: Korisnik preko standardnog ulaza unosi jedan ceo broj.
+ *   Broj koji treba pogoditi generisati “random” u opsegu od 1 do 50.
+ *   Vrteti se u petlji dok ne pogodimo broj, ili budemo barem blizu (ako je razlika između brojeva 5 ili manja).
+ *   Taj deo sa proverom pogotka izdvojiti u zasebnu metodu, kojoj se kao argument prosleđuje broj koji tipujemo,
+ *   a povratna vrednost metode je 0 – ako smo pogodili, 1 – ako smo bili blizu, 2 – ako smo promašili.
+ *   U zavisnosti od rezultata pogađanja ispisati i odgovarajuću poruku.
+ *   Nakon odigrane partije proveriti da li korisnik zeli da prekine ili da nastavi igru.
+ *
+ * @author Vladimir Krekic
+ */
 
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * AUTOR: Vladimir Krekic
- */
-
 class Pogadjanje {
 
     private static Scanner scanner = new Scanner(System.in);
     private static Random randomNumber = new Random();
-    private static int broj;
 
     public static void main(String[] args) {
 
-        while(true) {
-
+        do {
             //Generise random broj izmedju 1 i 50
-            int pogodi = randomNumber.nextInt(50)+1;
+            int pogodi = randomNumber.nextInt(50) + 1;
 
+            while (true) {
+
+                //Poziva metod za unos i proveru broja
+                int broj = unos();
+
+                //Proverava rezultat i pruza opcije korisniku u skladu sa rezultatom
+                int rezultat = provera(pogodi, broj);
+
+                if (rezultat == 0) {
+                    System.out.println("Pogodili ste");
+                        break;
+                } else if (rezultat == 1) {
+                    System.out.println("Blizu ste");
+                        break;
+                } else System.out.println("Niste pogodili. Pokusajte ponovo.");
+            }
+
+        }while(ponovo());
+
+        scanner.close();
+
+    }
+
+    //Unos broja i provera ispravnosti
+    private static int unos(){
+        boolean ispravno = true;
+        int broj = 0;
+        //Ponavlja unos sve dok ne bude unet ceo broj izmedju 1 i 50
+        while (ispravno){
             System.out.print("Upisi broj izmedju 1 i 50: ");
-            //Ponavlja unos sve dok ne bude unet ceo broj izmedju 1 i 50
             try {
                 broj = scanner.nextInt();
                 if(broj > 50 || broj < 1){
-                    System.out.println("Niste uneli broj izmedju 1 i 50");
+                    System.out.println("Niste uneli broj izmedju 1 i 50.\nPokusajte ponovo.");
                     continue;
                 }
             }catch (InputMismatchException e){
-                System.out.println("Niste uneli broj");
+                System.out.println("Niste uneli broj.\nPokusajte ponovo.");
+                //scanner.nextLine() proguta "Enter". Bez toga udje u beskonacnu petlju.
                 scanner.nextLine();
                 continue;
             }
-
-            //Proverava rezultat i pruza opcije korisniku u skladu sa rezultatom
-            int rezultat = provera(pogodi, broj);
-
-            if(rezultat==0){
-                System.out.println("Pogodili ste");
-                if (!ponovo())
-                    break;
-            }
-            else if(rezultat==1){
-                System.out.println("Blizu ste");
-                if (!ponovo())
-                    break;
-            }
-            else System.out.println("Niste pogodili. Pokusajte ponovo.");
-
+            ispravno = false;
         }
-
+        return broj;
     }
 
     //Proverava preciznost
